7e7eefd96f5325284d605da0de7509a9ae3f5b08
camiladamorim <camilabcdamorim@gmail.com>
2021-09-08 02:56:13 -0300

    idented

--- temproject/temproject/spiders/temp.py
+++ temproject/temproject/spiders/temp.py
@@ -6,62 +6,62 @@ import re
 
 class TempSpider(scrapy.Spider):
 
-    name = 'temp'
-    allowed_domains = ['youtube.com']
-    start_urls = ['https://www.youtube.com/results?search_query=tempest&sp=EgIQAg%253D%253D'] #so o 1 termo
+        name = 'temp'
+        allowed_domains = ['youtube.com']
+        start_urls = ['https://www.youtube.com/results?search_query=tempest&sp=EgIQAg%253D%253D'] #so o 1 termo
 
 
-    # lista_canais = ['tempest', 'youtube', 'estrela']
-    # for item in lista_canais:
-    #     url='https://www.youtube.com/results?search_query='+item+'&sp=EgIQAg%253D%253D'
-    #     start_urls = [url]
+        # lista_canais = ['tempest', 'youtube', 'estrela']
+        # for item in lista_canais:
+        #     url='https://www.youtube.com/results?search_query='+item+'&sp=EgIQAg%253D%253D'
+        #     start_urls = [url]
 
 
 
 
-    def parse(self, response):
-        
-        general_response = response.xpath('//*/text()').extract()  
+        def parse(self, response):
 
-        for i in range(len(general_response)):
-                if i==40:
-                        str_channels=general_response[i]
-                        list_channels=re.split('channelRenderer', str_channels)
-                        list_channel_1=re.split('\,', list_channels[1])
-                        for element in list_channel_1:
-                                if "shortBylineText" in element:
-                                        titulo =re.split("(\"text\"\:)", element)[2]
-                                        
-                                if "descriptionSnippet" in element: #
-                                        descricao =re.split("(\"text\"\:)", element)[2]
+                general_response = response.xpath('//*/text()').extract()  
 
-                                if "url" in element: 
-                                        url = re.split("(\"url\"\:)", element)[2]
+                for i in range(len(general_response)):
+                        if i==40:
+                                str_channels=general_response[i]
+                                list_channels=re.split('channelRenderer', str_channels)
+                                list_channel_1=re.split('\,', list_channels[1])
+                                for element in list_channel_1:
+                                        if "shortBylineText" in element:
+                                                titulo =re.split("(\"text\"\:)", element)[2]
+                                                
+                                        if "descriptionSnippet" in element: #
+                                                descricao =re.split("(\"text\"\:)", element)[2]
 
-                                if "thumbnails" in element:
-                                        img = re.split("(\"url\"\:)", element)[2]
+                                        if "url" in element: 
+                                                url = re.split("(\"url\"\:)", element)[2]
 
-                                if "searchEndpoint" in element:
-                                        query = str(re.split("^query(.*)", element)[0])
-                                        if "query" in query:
-                                                query = re.split("query\"\:\"", query)[1]
+                                        if "thumbnails" in element:
+                                                img = re.split("(\"url\"\:)", element)[2]
 
-                                if "channelId" in element:
-                                        id_ = str(re.split("^channelId(.*)", element)[0])
-                                        if "channelId" in id_:
-                                                id_ = re.split("channelId\"\:\"", id_)[1]
-                        
+                                        if "searchEndpoint" in element:
+                                                query = str(re.split("^query(.*)", element)[0])
+                                                if "query" in query:
+                                                        query = re.split("query\"\:\"", query)[1]
 
-        yield {
-                'titulo': titulo,
-                'descricao': descricao,
-                'url': url,
-                'img': img,
-                'query': query,
-                'id_': id_
-        }
+                                        if "channelId" in element:
+                                                id_ = str(re.split("^channelId(.*)", element)[0])
+                                                if "channelId" in id_:
+                                                        id_ = re.split("channelId\"\:\"", id_)[1]
+                                
 
-####################################################################
+                yield {
+                        'titulo': titulo,
+                        'descricao': descricao,
+                        'url': url,
+                        'img': img,
+                        'query': query,
+                        'id_': id_
+                }
+
+        ####################################################################
 
         # for div_quote in all_channels:
 
