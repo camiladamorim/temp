a75fe2922a0dbb57ccb33b19d1c936d1604aa7ff
camiladamorim <camilabcdamorim@gmail.com>
2021-09-08 11:10:58 -0300

    iterate trough keywords, stack items

--- temproject/temproject/spiders/temp.py
+++ temproject/temproject/spiders/temp.py
@@ -21,42 +21,65 @@ class TempSpider(scrapy.Spider):
                 general_response = response.xpath('//*/text()').extract()  
 
                 for i in range(len(general_response)):
-                        if i==40:
-                                str_channels=general_response[i]
-                                list_channels=re.split('channelRenderer', str_channels)
-                                list_channel_1=re.split('\,', list_channels[1])
-                                for element in list_channel_1:
-                                        if "shortBylineText" in element:
-                                                titulo =re.split("(\"text\"\:)", element)[2]
-                                                
-                                        if "descriptionSnippet" in element: #
-                                                descricao =re.split("(\"text\"\:)", element)[2]
+                        if i == 40:
+                                str_channels = general_response[i]
+                                list_all_channels = re.split('channelRenderer', str_channels) 
+                                #print("\n\n\n\n",list_all_channels)
 
-                                        if "url" in element: 
-                                                url = re.split("(\"url\"\:)", element)[2]
-
-                                        if "thumbnails" in element:
-                                                img = re.split("(\"url\"\:)", element)[2]
-
-                                        if "searchEndpoint" in element:
-                                                query = str(re.split("^query(.*)", element)[0])
-                                                if "query" in query:
-                                                        query = re.split("query\"\:\"", query)[1]
-
-                                        if "channelId" in element:
-                                                id_ = str(re.split("^channelId(.*)", element)[0])
-                                                if "channelId" in id_:
-                                                        id_ = re.split("channelId\"\:\"", id_)[1]
+                items = []
+                titulo, descricao, url, img, query, id_= '','','','','',''
                                 
+                for channel in list_all_channels:
+                        items_inside_each_channel = re.split('\,', channel)
 
-                yield {
-                        'titulo': titulo,
-                        'descricao': descricao,
-                        'url': url,
-                        'img': img,
-                        'query': query,
-                        'id_': id_
-                }
+                        for item in items_inside_each_channel:
+                                if "shortBylineText" in item:
+                                        titulo = re.split("(\"text\"\:)", item)[2]
+                                        
+                                if "descriptionSnippet" in item: #
+                                        descricao = re.split("(\"text\"\:)", item)[2]
+
+                                if "url" in item: 
+                                        url = re.split("(\"url\"\:)", item)[2]
+
+                                if "thumbnails" in item:
+                                        img = re.split("(\"url\"\:)", item)[2]
+
+                                if "searchEndpoint" in item:
+                                        query = str(re.split("^query(.*)", item)[0])
+                                        if "query" in query:
+                                                query = re.split("query\"\:\"", query)[1]
+
+                                if "channelId" in item:
+                                        id_ = str(re.split("^channelId(.*)", item)[0])
+                                        if "channelId" in id_:
+                                                id_ = re.split("channelId\"\:\"", id_)[1]
+                        
+                        json_item = {
+                                'titulo': titulo,
+                                'descricao': descricao,
+                                'url': url,
+                                'img': img,
+                                'query': query,
+                                'id_': id_ 
+                                }
+
+                        items.append(str(json_item))
+
+                print(items)
+                # yield {items}
+
+
+
+
+                # yield {
+                #         'titulo': titulo,
+                #         'descricao': descricao,
+                #         'url': url,
+                #         'img': img,
+                #         'query': query,
+                #         'id_': id_
+                # }
 
 
         # #mod
